e8d2e3ac75c4a3d2eec9773f360f96d2202b8150
ninja8247 <ninja8247@yahoo.com>
2021-05-17 14:38:05 -0400

    Update and rename turtletest001.txt to turtletest001.lua

--- turtletest001.lua
+++ turtletest001.lua
@@ -1,15 +1,19 @@
 function move(distance)
-	for moveblock=0,distance,1, do
+	for moveblock=0,distance,1
+	do
 		turtle.forward()
 	end
 return 0;
+end
 
 function goback()
 	moving = true
-	while moving = true, do
+	while moving = true
+	do
 		turtle.turnLeft()
 		turtle.turnLeft()
-		if turtle.getFuelLevel() < 11, do
+		if turtle.getFuelLevel() < 11
+		do
 			turtle.select(16)
 			turtle.refuel()
 			turtle.select(1)
@@ -22,18 +26,22 @@ function goback()
 	turtle.turnLeft()
 	turtle.turnLeft()
 return 0;
+end
 
 islighting = true
 
-while islighting = true, do
-	if turtle.getFuelLevel() < 11, do
+while islighting = true
+	do
+	if turtle.getFuelLevel() < 11
+	do
 		turtle.select(16)
 		turtle.refuel()
 		turtle.select(1)
 	end
 	move(11)
 	turtle.placeDown()
-	if turtle.detect()=true, do
+	if turtle.detect()=true
+	do
 		islighting = false
 	end
 end
